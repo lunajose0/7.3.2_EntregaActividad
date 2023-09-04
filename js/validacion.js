f798fcc5bdb46a09cfe11a08ba6f4dbd152f6da9
lunajose0 <josejavier.farias@gmail.com>
2023-09-03 21:21:44 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,27 +1,34 @@
 function validarFormulario() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var email = document.getElementById("email").value;
-    var contraseña = document.getElementById("password1").value;
-    var repetirContraseña = document.getElementById("password2").value;
-    var terminos = document.getElementById("terminos").checked;
-  
-    if (nombre === "" || apellido === "" || email === "" || contraseña === "" || repetirContraseña === "" || !terminos) {
-      showAlertError();
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  var email = document.getElementById("email").value;
+  var contraseña = document.getElementById("password1").value;
+  var repetirContraseña = document.getElementById("password2").value;
+  var terminos = document.getElementById("terminos").checked;
+
+  if (nombre === "" || apellido === "" || email === "" || contraseña === "" || repetirContraseña === "" || !terminos) {
+      showAlertError("Debe completar todos los campos.");
       return false;
-    }
-  
-    if (contraseña.length < 6) {
-      showAlertError();
-      return false;
-    }
-  
-    if (contraseña !== repetirContraseña) {
-      showAlertError();
-      return false;
-    }
-  
-    showAlertSuccess();
-    return true;
   }
+
+  if (contraseña.length < 6) {
+      showAlertError("La contraseña debe tener al menos 6 caracteres.");
+      return false;
+  }
+
+  if (contraseña !== repetirContraseña) {
+      showAlertError("Las contraseñas no coinciden.");
+      return false;
+  }
+
+  showAlertSuccess();
+  return true;
+}
+
+function showAlertError(mensaje) {
+  var alert = document.getElementById("alert-danger");
+  alert.innerHTML = mensaje;
+  alert.show();
+}
+
   
