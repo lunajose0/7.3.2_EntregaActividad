6ec6a0e071d2c4bd4514855c43d70f04276f6eb0
lunajose0 <josejavier.farias@gmail.com>
2023-09-03 21:18:08 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,27 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
+function validarFormulario() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    var contraseña = document.getElementById("password1").value;
+    var repetirContraseña = document.getElementById("password2").value;
+    var terminos = document.getElementById("terminos").checked;
+  
+    if (nombre === "" || apellido === "" || email === "" || contraseña === "" || repetirContraseña === "" || !terminos) {
+      showAlertError();
+      return false;
+    }
+  
+    if (contraseña.length < 6) {
+      showAlertError();
+      return false;
+    }
+  
+    if (contraseña !== repetirContraseña) {
+      showAlertError();
+      return false;
+    }
+  
+    showAlertSuccess();
+    return true;
+  }
+  
